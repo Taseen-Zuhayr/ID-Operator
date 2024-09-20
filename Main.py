3eda6919cce7f83904477b3582b5fcfba39ca250
Taseen-Zuhayr <taseen.zuhayr@gmail.com>
2024-09-20 22:37:47 +0600

    ID operator

---
+++ Main.py
@@ -0,0 +1,24 @@
+#1
+t = int(input("Enter here : "))
+
+if (type (t) is int):
+    print("True.")
+else:
+    print("False.")
+
+#2
+z = float(input("Enter here : "))
+
+if (type (z) is int):
+    print("True.")
+else:
+    print("False.")
+
+#3
+a = int(input("Enter here : "))
+b = int(input("Enter here : "))
+
+if a is b:
+    print("They are equal.")
+else:
+    print("They are not equal.")
